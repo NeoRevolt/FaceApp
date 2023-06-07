ceba7ccdec58c92d25cb311cb5521a7d1f3c1867
NeoRevolt <dzalfikriali@gmail.com>
2023-06-07 16:58:36 +0700

    Update to 3.2.1 FIX

--- faceappsdk/src/main/java/com/dartmedia/faceappsdk/utils/Const.kt
+++ faceappsdk/src/main/java/com/dartmedia/faceappsdk/utils/Const.kt
@@ -1,5 +1,6 @@
 package com.dartmedia.faceappsdk.utils
 
 object Const {
-    const val BASE_URL = "https://3d18-182-253-152-70.ngrok-free.app/"
+//    const val BASE_URL = "https://3d18-182-253-152-70.ngrok-free.app/"
+    const val BASE_URL = "http://172.105.121.90:9001/"
 }
