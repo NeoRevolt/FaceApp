aad651c0c5c83fba0c81e8447c02744fb13a10c1
NeoRevolt <dzalfikriali@gmail.com>
2023-06-20 11:22:09 +0700

    Update to 3.2.4 - add FACE_DISTANCE const - validation based by FACE_DISTANCE 0.35

--- faceappsdk/src/main/java/com/dartmedia/faceappsdk/FaceVerification.kt
+++ faceappsdk/src/main/java/com/dartmedia/faceappsdk/FaceVerification.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import com.dartmedia.faceappsdk.remote.ApiConfig
 import com.dartmedia.faceappsdk.remote.Status
 import com.dartmedia.faceappsdk.remote.response.VerifyResponseModel
+import com.dartmedia.faceappsdk.utils.Const
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.MultipartBody
 import okhttp3.RequestBody.Companion.asRequestBody
@@ -59,8 +60,6 @@ class FaceVerification {
         return isValid
     }
 
-
-    // Warning: Synchronous (handled with try-catch)
     fun verifySync(image1: File, image2: File): Status {
         isValid = Status.LOADING
         Log.d("SDK SYNCHRONOUS", "$isValid")
@@ -83,13 +82,14 @@ class FaceVerification {
             if (response.isSuccessful) {
                 Log.d("SDK", "Synchronous Call ")
                 val responseBody = response.body()
-                val verified = responseBody?.result?.verified
-                if (verified == true) {
+//                val verified = responseBody?.result?.verified
+                val distance = responseBody?.result?.distance as Double
+                if (distance <= Const.FACE_DISTANCE) {
                     isValid = Status.VALID
                 } else {
                     isValid = Status.INVALID
                 }
-                Log.d("SDK", "Distance : ${responseBody?.result?.distance}")
+                Log.d("SDK", "Distance : ${responseBody?.result?.distance}, $distance")
 
             } else if (response.code() >= 500) {
                 isValid = Status.SERVER_ERROR
@@ -106,4 +106,51 @@ class FaceVerification {
         Log.d("SDK SYNCHRONOUS", "$isValid")
         return isValid
     }
+
+    // Warning: Synchronous (handled with try-catch)
+//    fun verifySync(image1: File, image2: File): Status {
+//        isValid = Status.LOADING
+//        Log.d("SDK SYNCHRONOUS", "$isValid")
+//        val requestImageFile1 = image1.asRequestBody("image/*".toMediaTypeOrNull())
+//        val requestImageFile2 = image2.asRequestBody("image/*".toMediaTypeOrNull())
+//        val imageMultipart1: MultipartBody.Part = MultipartBody.Part.createFormData(
+//            "file1",
+//            image1.name,
+//            requestImageFile1
+//        )
+//        val imageMultipart2: MultipartBody.Part = MultipartBody.Part.createFormData(
+//            "file2",
+//            image2.name,
+//            requestImageFile2
+//        )
+//        val service = ApiConfig.getApiService().verifyImage(imageMultipart1, imageMultipart2)
+//        try {
+//            val response = service.execute()
+//            isValid = Status.LOADING
+//            if (response.isSuccessful) {
+//                Log.d("SDK", "Synchronous Call ")
+//                val responseBody = response.body()
+//                val verified = responseBody?.result?.verified
+//                if (verified == true) {
+//                    isValid = Status.VALID
+//                } else {
+//                    isValid = Status.INVALID
+//                }
+//                Log.d("SDK", "Distance : ${responseBody?.result?.distance}")
+//
+//            } else if (response.code() >= 500) {
+//                isValid = Status.SERVER_ERROR
+//            } else if (response.code() >= 400){
+//                isValid = Status.CLIENT_ERROR
+//            } else{
+//                isValid = Status.FAILED
+//            }
+//            Log.d("SDK", "Response : ${response.code()}")
+//        } catch (e: Exception) {
+//            Log.d("SDK", "$e")
+//            isValid = Status.EXCEPTION
+//        }
+//        Log.d("SDK SYNCHRONOUS", "$isValid")
+//        return isValid
+//    }
 }
--- faceappsdk/src/main/java/com/dartmedia/faceappsdk/utils/Const.kt
+++ faceappsdk/src/main/java/com/dartmedia/faceappsdk/utils/Const.kt
@@ -4,4 +4,5 @@ object Const {
 //    const val BASE_URL = "https://3d18-182-253-152-70.ngrok-free.app/"
 //    const val BASE_URL = "http://172.105.121.90:9001/"
     const val BASE_URL = "http://172.105.121.90/api/facematch/v1/"
+    const val FACE_DISTANCE = 0.35
 }
